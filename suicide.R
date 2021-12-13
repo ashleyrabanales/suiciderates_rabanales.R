66d422913057fcdc7183b92731f66be894a08c52
ashleyrabanales <arabana2@students.kennesaw.edu>
2021-12-13 03:24:42 -0500

    commit

--- suicide.R
+++ suicide.R
@@ -5,8 +5,7 @@ library(ggplot2)
 library(statar)
 library(gapminder)
 library(RColorBrewer)
-library(hrbrthemes)
-library(viridis)
+
 #install.packages("viridis")
 
 httpgd::hgd()
@@ -29,11 +28,7 @@ tab(dat, sex)
 sum_up(dat, suicides)
 
 dat_2 <- dat %>%
-    mutate(
-        new_year = ifelse(year < 2000, "before2000", "after2000") %>%
-        factor(levels = c("before2000", "after2000")) #default sorts by name
-    ) %>%
-  group_by(sex, age, country, generation, population) %>%
+  group_by(sex, age, country, generation, population)%>%
   summarize(suicides_pop = n())
 
 head(dat_2)
@@ -58,46 +53,9 @@ ggplot(data = dat_2) +
 ggsave(file = "generation.png", width = 15, height = 7)
 
 
-#####line graph
-#creating a weightmean for gdp per captia
-dat_count <- gapminder :: gapminder %>%
-     mutate(
-        new_year = ifelse(year < 2000, "before2000", "after2000") %>%
-        factor(levels = c("before2000", "after2000")) #default sorts by name
-    ) %>%
-    group_by(new_year, country) %>%
-    summarise(
-      gdp_per_cap = weighted.mean(gdp_per_cap, suicides_pop),
-      population = sum(population)) %>%
-  ungroup() %>%
-
-  #group_by(sex, age, country, generation, dat_count) %>%
-  #summarize(suicides_pop = n()) 
-
-dat3 <- dat %>%
-group_by(sex, age, country, generation, dat_count, new_year) %>%
-
-ggplot(dat, mapping = aes (x = year, y = gdp_per_cap,
-                color = continent)) + 
-                geom_line(aes(group=country)) + 
-                geom_point() +
-                facet_wrap(~country, nrow = 1) +
-                geom_point(aes(size = pop/100000)) +
-                geom_point(data = dat_count,aes(size=1,color="black") +
-                geom_line(data = dat_count, color="black") + 
-                labs( x="Year", y="GDP per capita", 
-                size = "Population (100k)" , color = "Continent") +
-                scale_y_continuous(trans = "sqrt") +
-                theme_bw() 
-        
-ggsave(filename = "p1_Rproject2.png", width = 15)
-#might too big to make
-
 ###########
 
-https://www.r-graph-gallery.com/135-stacked-density-graph.html
-
-
+#https://www.r-graph-gallery.com/135-stacked-density-graph.html
 #library(gapminder)
 #library(viridis)
 
@@ -105,26 +63,73 @@ tab(dat, year)
 tab(dat, suicides_pop)
 tab()
 
-dat3 gapminder::gapminder %>% filter(year=="2000") %>% dplyr::select(-year)
-head(dat3)
-# Most basic bubble plot
+#dat3 gapminder::gapminder %>% filter(year=="2000") %>% dplyr::select(-year)
+head(dat)
+#filtering out the year 2000 and 2016 
+dat3 <- dat %>%
+  filter(year == "2016")%>%
+  group_by(sex, age, country, generation, population) %>%
+
+
+ggplot(data = dat3)
+  geom_bar(mapping = aes(x = suicides_pop, fill = age)) +
+  scale_discrete_continuos +
+  scale_fill_brewer(palette="Pastel2") +
+  theme_bw() +
+  labs(
+    title = "Suicide by Age in 2000",
+    caption = "Suicide in the Twenty-First Century",
+    tag = "Figure 2:",
+    x = "Suicides Population 100k",
+    y = "Count",
+    fill = "Age") +
+  theme(plot.title = element_text(face="bold", size=14
+  )) + theme(plot.tag = element_text(face="bold", size=14
+  ))
+
+
 dat %>%
-  filter(year=="2016") %>%
-  ggplot(aes(x=gdp_per_capita*100000, y=population*100000, size = suicides_pop, color = age)) +
+filter(year=="2016") %>%
+ggplot(data = dat) 
+  geom_smooth(mapping = aes(x = suicides_pop, y = gpd_per_capita, linetype = age))
+
+  geom_bar(dat, mapping = aes(x = suicides_pop, fill = age)) +
+  scale_fill_brewer(palette="Pastel2") +
+  theme_bw() +
+  labs(
+    title = "Suicide by Age in 2016",
+    caption = "Suicide in the Twenty-First Century",
+    tag = "Figure 3:",
+    x = "Suicides Population 100k",
+    y = "Count",
+    fill = "Age") +
+  theme(plot.title = element_text(face="bold", size=14
+  )) + theme(plot.tag = element_text(face="bold", size=14
+  ))
+
+#######################################
+#code wasn;t working, not sure what went wrong
+
+dat %>%
+  filter(year=="2016") %>% 
+  ggplot(aes(x=gdp_per_capita*100000, y=population, size = suicides_pop, color = age)) +
     geom_point(alpha=0.5) +
-    scale_size(range = c(.1, 24), name="Population ") + scale_fill_viridis(discrete=TRUE, guide=FALSE, option="A") +
+    scale_size(range = c(.1, 24), name="Population ") +
+     scale_fill_viridis(discrete=TRUE, guide=FALSE, option="A") +
     theme_ipsum() +
-    theme(legend.position="bottom") +
-    ylab("Life Expectancy") +
-    xlab("Gdp per Capita") +
+    title = ""
+    theme(legend.position="bottom") 
+    + labs( x="Gdp per Capita", y="", 
+          size = "Suicide Population (100k)" , color = "Age") +
+        guide = "none"+
     theme(legend.position = "none")
 
 dat %>%
   filter(year=="2000") %>%
-  ggplot(aes(x=gdp_per_capita*100000, y=population*100000, size = suicides_pop, color = age)) +
-    geom_point(alpha=0.5, shape=21, color="black") +
-    scale_size(range = c(.1, 25), name="Population (M)") +
-     scale_fill_viridis(discrete=TRUE, guide=FALSE, option="A") +
+  ggplot(aes(x=gdp_per_capita*100000, y=suicides_pop*100000, size = suicides_pop, color = age)) +
+    geom_point(alpha=0.5) +
+    scale_size(range = c(.1, 24), name="Population") +
+    scale_fill_viridis(discrete=TRUE, guide=FALSE, option="A") +
     theme_ipsum() +
     theme(legend.position="bottom") +
     ylab("Life Expectancy") +
@@ -134,3 +139,37 @@ dat %>%
 
 
 ggsave(file = "fig_3R.png", width = 8, height = 4)
+
+
+
+
+#####line graph
+#creating a weightmean for gdp per captia
+#dat_count <- gapminder :: gapminder %>%
+#     mutate(
+#        new_year = ifelse(year < 2000, "before2000", "after2000") %>%
+#        factor(levels = c("before2000", "after2000")) #default sorts by name # nolint
+ #   ) %>%
+ #   group_by(new_year, country) %>%
+ #   summarise(
+ #     gdp_per_cap = weighted.mean(gdp_per_cap, suicides_pop),
+ #     population = sum(population)) %>%
+ # ungroup() %>%
+
+  #group_by(sex, age, country, generation, dat_count) %>%
+  #summarize(suicides_pop = n()) 
+
+ # ggplot(dat, mapping = aes (x = year, y = gdp_per_cap,
+  #              geom_line(aes(group=country)) + 
+   #             geom_point() +
+   #             facet_wrap(~country, nrow = 1) +
+    #            geom_point(aes(size = pop/100000)) +
+   #             geom_point(data = dat_count,aes(size=1,color="black") +
+   #             geom_line(data = dat_count, color="black") + 
+    #            labs( x="Year", y="GDP per capita", 
+    #            size = "Population (100k)" , color = "Continent") +
+   #             scale_y_continuous(trans = "sqrt") +
+    #            theme_bw() 
+        
+#ggsave(filename = "p1_Rproject2.png", width = 15)
+#might too big to make
